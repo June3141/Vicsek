6e215fd78d66979f9dc2bb2a0ae451aa21db3061
Kosei Himeno <k.himeno314@gmail.com>
2016-06-16 19:49:59 +0900

    initialize

---
+++ Vicsek.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+const_velocity = 0
+
+def theta():
+    print("HELLO")
+    pass
+
+def moving_radius():
+    pass
+
+
+def _angle_average():
+    pass
+
+def _white_noise():
+    pass
+
+
+def _unit_of_angle():
+    pass
---
+++ main.py
@@ -0,0 +1,5 @@
+import Vicsek as vicsek
+
+
+if __name__ == '__main__':
+    vicsek.t heta()
