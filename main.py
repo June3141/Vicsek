1a892727be8d6f54587f40258addd896b7ba058e
Kosei Himeno <k.himeno314@gmail.com>
2016-06-16 22:06:35 +0900

    パラメータを追加

--- main.py
+++ main.py
@@ -8,24 +8,25 @@ import os
 def scatter_plot(x_array, y_array, time, base_path=None):
     plt.cla()
     plt.clf()
-    plt.title("TIME = {time}".format(time=time))
+    plt.title("No: {time}".format(time=time))
     plt.scatter(x_array, y_array)
     save_path = os.path.join(base_path, "vicsek-{time}.png".format(time=time))
     plt.savefig(save_path)
-    print("TIME = {time} の時刻を出力しました。".format(time=time))
+    print("No. {time}を出力しました。".format(time=time))
     # plt.show()
 
 
 if __name__ == '__main__':
-    update_number = 1
-
-    objs = []
-
-    x_time_y = [15, 5]
-
+    # パラメータの設定
+    x_time_y = [20, 20]
     x_pos_array = []
     y_pos_array = []
+    vicsek.CONST_VELOCITY = 0.0001
+    vicsek.THRESHOLD_RADIUS = 2
+    time_coeff = 100
 
+    # ----------------------------------------
+    objs = []
     for x_pos in np.arange(x_time_y[0]):
         for y_pos in np.arange(x_time_y[1]):
             obj = vicsek.SPP(x_pos + 0.01, y_pos + 0.01)
@@ -40,7 +41,7 @@ if __name__ == '__main__':
         y_pos_array = []
         after_obj = []
         for obj in objs:
-            obj.update(objs, time=time)
+            obj.update(objs, time=time / time_coeff)
             after_obj.append(obj)
             x_pos_array.append(obj.pos_x)
             y_pos_array.append(obj.pos_y)
